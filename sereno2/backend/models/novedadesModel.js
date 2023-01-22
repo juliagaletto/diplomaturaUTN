12bd5df6b9f920a353ed3b6cf36e431dbbccc2d2
juliagaletto <tradjuliagaletto@gmail.com>
2023-01-22 22:06:57 +1100

    M6U1

---
+++ sereno2/backend/models/novedadesModel.js
@@ -0,0 +1,21 @@
+var pool = require('./bd');
+
+async function getNovedades() {
+    var query = 'select * from promociones';
+    var rows = await pool.query(query);
+    return rows;
+}
+
+async function insertNovedad(obj) {
+    try {
+    var query = "insert into promociones set ? ";
+    var rows = await pool.query(query, [obj]);
+    return rows;
+} catch(error) {
+    console.log(error);
+    throw error;
+}
+    
+}
+
+module.exports = { getNovedades, insertNovedad }
